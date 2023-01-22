0eecde88b80d039b57039d3ca6e75310695f23ad
Brett Sumser <bsumser@gmail.com>
2023-01-21 16:31:31 -0800

    working black image output

--- proj1B.c
+++ proj1B.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*------------------------STARTER CODE-------------------------------------------*/
 double C441(double f)
 {
     return ceil(f-0.00001);
@@ -58,12 +59,108 @@ GetTriangles(void)
 
    return tl;
 }
+/*------------------------END STARTER CODE-------------------------------------------*/
+
+typedef struct
+{
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+}Pixel;
 
 
+typedef struct
+{
+    Pixel pixels[1000][1000];
+}Image;
 
-/* some useful code that goes in your main loop:
-    //for (int i = 0 ; i < tl->numTriangles ; i++)
+//declare all pixel colors used
+Pixel black = {.red = 0, .green = 0, .blue = 0};
+Pixel grey = {.red = 128, .green = 128, .blue = 128};
+Pixel white = {.red = 255, .green = 255, .blue = 255};
+Pixel red = {.red = 255, .green = 0, .blue = 0};
+Pixel green = {.red = 0, .green = 255, .blue = 0};
+Pixel blue = {.red = 0, .green = 0, .blue = 255};
+Pixel pink = {.red = 255, .green = 0, .blue = 255};
+Pixel cyan = {.red = 0, .green = 255, .blue = 255};
+Pixel yellow = {.red = 255, .green = 255, .blue = 0};
+
+
+void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
+{
+    printf("triangle color rgb values are %d %d %d \n",
+           triangle->color[0], triangle->color[1], triangle->color[2]);
+    printf("triangle vertexes are (%f,%f) (%f,%f) (%f,%f)",
+           triangle->X[0], triangle->Y[0], triangle->X[1], triangle->Y[1], triangle->X[2], triangle->Y[2]);
+}
+
+void writeImage(Image image, FILE *fp)
+{
+    int num = 1000 * 1000;
+    fwrite(&image, sizeof(Image), num, fp);
+}
+
+
+FILE* setFile(int width, int height, int colorRange)
+{
+    char *filename = "proj1B_out.pnm";
+    FILE *fp = fopen(filename, "w");
+
+    if (fp == NULL) {
+        printf("file open failure\n");
+        return 0;
+    }
+    else {
+        printf("file open success\n");
+    }
+
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "%d\n", colorRange);
+
+    return fp;
+}
+
+int main(int argc, char* argv[])
+{
+    printf("triangulator running\n");
+
+    int width = 1000;
+    int height = 1000;
+    int colorRange = 255;
+
+    TriangleList *tl = GetTriangles();
+
+    Image img;
+
+    for (int i; i < 1000; i++) {
+        for (int j; j < 1000; j++) {
+            img.pixels[i][j] = black;
+        }
+    }
+
+    char *filename = "proj1B_out.pnm";
+    FILE *fp = fopen(filename, "w");
+
+    if (fp == NULL) {
+        printf("file open failure\n");
+        return 0;
+    }
+    else {
+        printf("file open success\n");
+    }
+
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%d %d\n", width, height);
+    fprintf(fp, "%d\n", colorRange);
+
+    writeImage(img, fp);
+
+//some useful code that goes in your main loop:
+    for (int i = 0 ; i < tl->numTriangles ; i++) {
         //RasterizeGoingUpTriangle(tl->triangles+i, &img);
-    for (int i = 0 ; i < 1 ; i++)
-        RasterizeGoingUpTriangle(tl->triangles+i, &img);
- */
+    }
+
+    fclose(fp);
+    return 0;
+}
