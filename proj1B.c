7deebfef7b277ebe1acdf99ce5ddbd0bdedaf8ae
Brett Sumser <bsumser@gmail.com>
2023-01-24 19:14:39 -0800

    new triangle members, saves vertex pos, determine triangle

--- proj1B.c
+++ proj1B.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 /*------------------------STARTER CODE-------------------------------------------*/
 double C441(double f)
 {
@@ -15,9 +16,16 @@ double F441(double f)
 
 typedef struct
 {
-   double         X[3];
-   double         Y[3];
-   unsigned char color[3];
+    double         X[3];
+    double         Y[3];
+    unsigned char color[3];
+
+    //added in for tracking index pos
+    int leftIdx;
+    int rightIdx;
+    int topIdx;
+    int bottomIdx;
+
 } Triangle;
 
 typedef struct
@@ -55,6 +63,11 @@ GetTriangles(void)
        tl->triangles[i].color[0] = colors[i%6][0];
        tl->triangles[i].color[1] = colors[i%6][1];
        tl->triangles[i].color[2] = colors[i%6][2];
+
+       tl->triangles[i].leftIdx = 0;
+       tl->triangles[i].rightIdx = 0;
+       tl->triangles[i].topIdx = 0;
+       tl->triangles[i].bottomIdx = 0;
    }
 
    return tl;
@@ -85,19 +98,17 @@ Pixel pink = {.red = 255, .green = 0, .blue = 255};
 Pixel cyan = {.red = 0, .green = 255, .blue = 255};
 Pixel yellow = {.red = 255, .green = 255, .blue = 0};
 
-
-void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
-{
+//given 3 vertices, determine type of triangle and orientation of vertices
+void determineTriangle(Triangle *triangle) {
     printf("triangle color rgb values are %d %d %d \n",
            triangle->color[0], triangle->color[1], triangle->color[2]);
     printf("triangle vertexes are (%f,%f) (%f,%f) (%f,%f)\n",
            triangle->X[0], triangle->Y[0], triangle->X[1], triangle->Y[1], triangle->X[2], triangle->Y[2]);
 
-    Pixel pixel = {.red = triangle->color[0], .green = triangle->color[1], .blue = triangle->color[2]};
-
     int leftIdx = 0;
     int topIdx = 0;
     int rightIdx = 0;
+    int bottomIdx = 0;
     for (int i = 0; i < 3; i++) {
         if (triangle->X[leftIdx] >= triangle->X[i] && triangle->Y[leftIdx] >= triangle->Y[i]) {
             leftIdx = i;
@@ -105,29 +116,51 @@ void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
         if (triangle->Y[topIdx] <= triangle->Y[i]) {
             topIdx = i;
         }
+        if (triangle->Y[bottomIdx] >= triangle->Y[i]) {
+            bottomIdx = i;
+        }
     }
     rightIdx = 6 - (topIdx + leftIdx + 3);
+
+    triangle->leftIdx = leftIdx;
+    triangle->rightIdx = rightIdx;
+    triangle->topIdx = topIdx;
+    triangle->bottomIdx = bottomIdx;
 
     printf("left vertex index is %d at (%f,%f)\n", leftIdx, triangle->X[leftIdx], triangle->Y[leftIdx]);
     printf("top vertex index is %d at (%f,%f)\n", topIdx, triangle->X[topIdx], triangle->Y[topIdx]);
     printf("right vertex index is %d at (%f,%f)\n", rightIdx, triangle->X[rightIdx], triangle->Y[rightIdx]);
+    printf("bottom vertex index is %d at (%f,%f)\n", bottomIdx, triangle->X[bottomIdx], triangle->Y[bottomIdx]);
 
-    double minX = F441(triangle->X[leftIdx]);
-    double maxX = C441(triangle->X[rightIdx]);
-    double minY = F441(triangle->Y[leftIdx]);
-    double maxY = C441(triangle->Y[topIdx]);
+    //flat bottom
+
+    //flat top
+
+    //arbitrary
+
+}
+
+void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
+{
+    determineTriangle(triangle);
+    Pixel pixel = {.red = triangle->color[0], .green = triangle->color[1], .blue = triangle->color[2]};
+
+    double minX = F441(triangle->X[triangle->leftIdx]);
+    double maxX = C441(triangle->X[triangle->rightIdx]);
+    double minY = F441(triangle->Y[triangle->leftIdx]);
+    double maxY = C441(triangle->Y[triangle->topIdx]);
 
     printf("X floor is %f ceil is %f\n", minX, maxX);
     printf("Y floor is %f ceil is %f\n", minY, maxY);
 
     printf("Scanlines go from %1f to %1f\n", minY, maxY);
 
-    double x1 = triangle->X[topIdx];
-    double y1 = triangle->Y[topIdx];
-    double x2 = triangle->X[leftIdx];
-    double y2 = triangle->Y[leftIdx];
-    double x3 = triangle->X[rightIdx];
-    double y3 = triangle->Y[rightIdx];
+    double x1 = triangle->X[triangle->topIdx];
+    double y1 = triangle->Y[triangle->topIdx];
+    double x2 = triangle->X[triangle->leftIdx];
+    double y2 = triangle->Y[triangle->leftIdx];
+    double x3 = triangle->X[triangle->rightIdx];
+    double y3 = triangle->Y[triangle->rightIdx];
 
     double slopeRight = (y3 - y1)/(x3 - x1);
     if (slopeRight == INFINITY) {printf("right infinity achieved\n");}
@@ -145,7 +178,7 @@ void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
 
         printf("inserting pixel at pixels[%d][%d]\n", x, y);
 
-        if (x >= 1000 || y >= 1000)
+        if ((x >= 1000 || y >= 1000) || (x < 0 || y < 1000))
             continue;
         img->pixels[x][y] = pixel;
     }
@@ -155,6 +188,7 @@ void RasterizeGoingUpTriangle(Triangle *triangle, Image *img)
 
     }
 }
+
 
 void writeImage(Image image, FILE *fp)
 {
@@ -217,7 +251,7 @@ int main(int argc, char* argv[])
 
 
 //some useful code that goes in your main loop:
-    for (int i = 0 ; i < tl->numTriangles; i++) {
+    for (int i = 0 ; i < 1/*tl->numTriangles*/; i++) {
         RasterizeGoingUpTriangle(tl->triangles+i, &img);
     }
 
