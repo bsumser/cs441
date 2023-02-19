d106db85289e5c379cb70d196d53049d1a105f50
Brett Sumser <bsumser@gmail.com>
2023-02-19 14:31:08 -0800

    window opening

--- proj/main.cpp
+++ proj/main.cpp
@@ -3,9 +3,11 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 //calllback function when resizing window
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+using namespace glm;
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +16,13 @@ int main(int argc, char *argv[])
     int width = 1024;
     int height = 768;
 
+// Initialise GLFW
+    glewExperimental = true; // Needed for core profile
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -29,22 +38,32 @@ int main(int argc, char *argv[])
         return -1;
     }
     glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    glewExperimental = true;
 
     glViewport(0, 0, width, height);
 
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to init GLEW\n");
+        return -1;
     }
 
-    glfwTerminate();
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do{
+        // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Draw nothing, see you in tutorial 2 !
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+       glfwWindowShouldClose(window) == 0 );
+
     return 0;
 }
 
